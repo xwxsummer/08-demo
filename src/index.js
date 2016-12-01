a6fd227de7296bd66b85ceeb47ba57f74c914393
xwxsummer <xiagh21@163.com>
2016-12-01 16:25:23 +0800

    deploy

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-
-import Routers from './routers';
-import './main.css';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-injectTapEventPlugin();
-render(<Routers />,document.getElementById('root'));
